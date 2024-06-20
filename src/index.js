e6a00667d2ee116bf288def5f65fb3e72abb05dc
Bharat Dua <bharatdua13@gmail.com>
2024-06-20 12:31:04 +0530

    profile card using react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar image="images/nature.jpg" />
+      <div className="data">
+        <Intro
+          name="Bharat Dua"
+          description="a full stack developer with good knowlege on the client side technologies i.e. HTML,CSS,JS,REACT,BOOTSTRAP,TAILWIND,MUI as well as on the server side technologies i.e. Node.js,Express.js,MongoDB,Postman,JWT"
+        />
+        <Skillset />
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return <img src={props.image} className="avatar" alt="avatar" />;
+}
+function Intro(props) {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+function Skillset(props) {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Node.js" emoji="👶" color="orangered" />
+    </div>
+  );
+}
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
